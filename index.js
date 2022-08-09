63fa43d33225ab6bc97ea6b8db7a91916e9aa882
Dave Fisher <dave.j.fish@gmail.com>
2022-08-09 10:20:34 -0700

    working tests for all class methods

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(list) {
+    this.#list = list;
+  }
+
+  push(prop) {
+    // adds item to list
+    this.#list.push(prop);
+  }
+
+  pop() {
+    // removes and returns item from list
+    return this.#list.pop();
+  }
+
+  peek() {
+    // returns last item in list
+    return this.#list[this.#list.length - 1];
+  }
+
+  get list() {
+    return this.list
+  }
+}
 
 class Queue {}
 
--- index.test.js
+++ index.test.js
@@ -1,9 +1,21 @@
 const { Stack } = require("./index.js");
 describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
-    const stack = new Stack();
+    const stack = new Stack([1, 2, 3]);
     stack.push(1);
     expect(stack.peek()).toEqual(1);
   });
-  // add more tests here...
+  
+  it('#pop should remove and return the last item from list', () => {
+    const stack = new Stack([1, 2, 3]);
+    const poppedItem = stack.pop();
+    expect(poppedItem).toEqual(3);
+    expect(stack.peek()).toEqual(2);
+  });
+
+  it('#peek should return the last item in the list array', () => {
+    const stack = new Stack(['apples', 'oranges', 'pears']);
+    console.log('here is the peek value', stack.peek())
+    expect(stack.peek()).toEqual('pears');
+  });
 });
